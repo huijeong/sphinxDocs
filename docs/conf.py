405fd2ec90699b0d8551ccaf5fe441e956f257cd
huijeong <huijeong.heo@dongobi.com>
2023-03-17 15:51:10 +0900

    edit

--- docs/conf.py
+++ docs/conf.py
@@ -29,6 +29,9 @@ author = "Development part core team"
 # -- General configuration ---------------------------------------------------
 # -- General configuration
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 extensions = [
     "sphinx.ext.duration",
